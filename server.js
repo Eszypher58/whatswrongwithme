e228f2e982d317addc7f8a09e0f6b765bd029bf7
stanley <chw058@gmail.com>
2017-10-08 12:20:38 -0700

    simple frontend to play back multiple files

--- server.js
+++ server.js
@@ -6,20 +6,39 @@ var fs = require("fs");
 var app = express();
 var port = 8080;
 
+//testing purpose
+//var wavLocation = [path.join(__dirname + '/1507479996513.wav'), path.join(__dirname + '/1507481993009.wav')];
+//var wavLocation = [{ id:0, name:'1507479996513.wav'}, {id:1, name:'1507481993009.wav'}];
+var wavLocation = ['1507479996513.wav', '1507481993009.wav'];
 
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.raw({ type: 'audio/wav', limit: '50mb' }));
 
-app.use(express.static('audio_test.html'));
+app.use(express.static("wav_file"));
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 app.get('/', function(req, res){
 
     console.log("hit get");
 
-    res.sendFile(path.join(__dirname + "/audio_test.html"));
+    //res.sendFile(path.join(__dirname + "/audio_test.html"));
+    //res.json(wavLocation);
+    res.render("index", {data: wavLocation});
 
 })
 
+app.get("/data.json", function(req, res){
+
+    console.log("hit get data.json");
+    res.json(wavLocation);
+
+})
+
+//when user post sound data, server process it by saving as wav file. wav file is named using number of milisecond since epoch to avoid conflict with same file name
 app.post('/', function (req, res) {
   console.log("audio recived", req.body);
 
@@ -28,12 +47,6 @@ app.post('/', function (req, res) {
   var now = Date.now();
   var fileName = now + ".wav";
 
-  //if(!recorder) return alert('No recording found.');
-/*
-  var file = new File([audioBlob], fileName, {
-      type: "audio/wav"
-  });
-*/
   fs.writeFile(fileName, audioBlob, function(err){
 
     if (err) {
@@ -41,8 +54,8 @@ app.post('/', function (req, res) {
         return console.log(err);
 
     }
-
-    res.end("posted something")
+    res.redirect("/");
+    res.end("posted something");
 
   })
 
